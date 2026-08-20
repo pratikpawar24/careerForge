6b720ae30e19bf17c859e0e7072ce8f1ee71db2f
Pratik Pawar <pratikpawarpune@gmail.com>
2026-08-20 20:01:04 +0530

    first commit

---
+++ src/main/java/com/careerforge/careerforge/CareerforgeApplication.java
@@ -0,0 +1,13 @@
+package com.careerforge.careerforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerforge/careerforge/CareerforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerforge.careerforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
